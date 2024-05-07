8628706a448c316327746b1892382a7206fc3854
LHS <lyh903l@naver.com>
2024-05-07 16:01:09 +0900

    concat_sheet_excel 2024/05/07

--- concat_sheet_excel.py
+++ concat_sheet_excel.py
@@ -1,29 +1,31 @@
+# excel sheet 결합 코드
+
 import os
 import pandas as pd
 
-# Directory containing Excel files
+# 디렉터리 경로 지정
 directory = "/home/aptech/projects/test/data/excel/data1.xlsx"
 
-# Create an empty DataFrame to store concatenated data
+# 빈 DataFrame 생성
 all_temp_data = pd.DataFrame()
 
-# Iterate over each file in the directory
+# 디렉터리 내의 모든 파일에 대해 반복
 for filename in directory:
-    if filename.endswith(".xlsx"):  # Check if file is Excel file
-        # Import Excel file
+    if filename.endswith(".xlsx"):  # Excel 파일만 선택
+        # Excel 파일 불러오기
         filepath = os.path.join(directory, filename)
         data = pd.read_excel(filepath, sheet_name=None, engine='openpyxl')
 
-        # Create a single DataFrame by vertically connecting only the temperature data on all sheets
+        # 각 시트에서 온도 데이터만 선택
         temp_data_per_file = pd.DataFrame()
         for sheet_name, df in data.items():
-            temp_data = df.iloc[:10000, 1]  # Select only 2nd column for temperature data
+            temp_data = df.iloc[:10000, 1]  # 1열(온도 데이터)만 선택
             temp_data_per_file = pd.concat([temp_data_per_file, temp_data], axis=1)
 
-        # Concatenate data from each file horizontally
+        # 모든 파일의 데이터를 하나로 연결
         all_temp_data = pd.concat([all_temp_data, temp_data_per_file], axis=1)
 
-# Save the concatenated data to a new Excel file
+# 모든 데이터를 하나의 Excel 파일로 저장
 output_filepath = r"C:\Users\MSI\Desktop\회사\개발\MLTA\dataset\company_data\trace_data_excel\concat_time_60\concat_time_60_2.xlsx"
 all_temp_data.to_excel(output_filepath, index=False, header=False)
 
