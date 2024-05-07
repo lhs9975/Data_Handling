79e8a6137e5e381b073bce9e88c99495c60d9ac1
LHS <lyh903l@naver.com>
2024-05-07 16:01:28 +0900

    data_transpose 2024/05/07

--- data_transpose.py
+++ data_transpose.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 # CSV 파일 불러오기
 data = pd.read_csv(r"C:\Users\MSI\Desktop\회사\개발\MLTA\company_data\trace_data_excel\tra_abnormal_concat_10_2\abnormal_concat_10_2.csv",
-                   header=None)  # 'input.csv'는 사용자의 CSV 파일명입니다.
+                   header=None)  # 'input.csv'는 사용자의 CSV 파일명
 
 # 행과 열 반전
 transposed_data = data.transpose()
 
 # 새로운 CSV 파일로 저장
 transposed_data.to_csv(r"C:\Users\MSI\Desktop\회사\개발\MLTA\company_data\trace_data_excel\tra_abnormal_concat_10_2\abnormal_concat_10_2_T.csv",
-                       index=False, header=False, encoding='utf-8')  # 'output.csv'는 저장할 CSV 파일명입니다.
+                       index=False, header=False, encoding='utf-8')  # 'output.csv'는 저장할 CSV 파일명
